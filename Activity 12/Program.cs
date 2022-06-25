75871e1d6608fe21072dfa5e11a2320510017994
Christopher Peterson <CPeterson95@my.gcu.edu>
2022-06-24 21:00:35 -0500

    Add project files.

---
+++ Activity 12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+
+
+String input;
+try
+{
+    StreamReader sr = new StreamReader("input.txt");
+
+    int total = 0;
+    input = sr.ReadToEnd();
+    input = new string(input.Where(c => !char.IsPunctuation(c)).ToArray());
+    char[] chars = input.ToLower().ToCharArray();
+
+    for (int i = 0; i < chars.Length; i++)
+    {
+        if (chars[i] == ' ')
+            if (chars[i - 1] == 't' || chars[i - 1] == 'e')
+                total++;
+    }    
+
+    Console.WriteLine("There are " + total + " words that end in t and e"); 
+    
+    sr.Close();
+}
+catch (Exception e)
+{
+    Console.WriteLine("Exception: " + e.Message);
+}
